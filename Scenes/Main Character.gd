33b05f11615405d75fc7e697c71b9981db747b2d
Diced Orange <travisbergen8@gmail.com>
2024-01-15 17:19:13 -0700

    movement update

--- Scenes/Main Character.gd
+++ Scenes/Main Character.gd
@@ -1,15 +1,30 @@
-extends Node2D
+extends CharacterBody2D
 
 
-@export var speed = 100
+const max_speed = 400
+const acceleration = 1000
+const friction = 800
 
-@export var fall_acceleration = 75
+var input = Vector2.ZERO
+
+func _physics_process(delta):
+	player_movement(delta)
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	var velocity = input_direction * speed
+	input.x = int(input.is_action_pressed("ui_right")) - int(input.is_action_pressed("ui_left"))
+	input.y = int(input.is_action_pressed("ui_down")) - int(input.is_action_pressed("ui_up"))
+	return input.normalized()
 
-
-func _physics_process(_delta):
-	get_input()
-	move_and_slide()
+func player_movement(delta):
+	input = get_input()
+	
+	if input == Vector2.ZERO:
+		if velocity.length() > (friction * delta):
+			velocity -= velocity.normalized() * (friction * delta)
+		else:
+			velocity = Vector2.ZERO
+	else:
+		velocity += (input * delta * acceleration)
+		velocity = velocity.limit_length(max_speed)
+		
+		move_and_slide()
